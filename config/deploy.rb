eb3cd6389b1138889748064d6a97edc6e0a64f06
Alexander Markov <apsheronets@gmail.com>
2016-11-13 21:29:00 +0300

    capistrano: using our branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,7 @@ set :repo_url, "git@github.com:apsheronets/#{fetch :application}.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-#set :branch, :capistrano
-set :branch, :daemons
+set :branch, :capistrano
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'

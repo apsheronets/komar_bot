b345d09e177c750811b562e327c77d9613f7b471
Alexander Markov <apsheronets@gmail.com>
2016-11-13 21:31:39 +0300

    capistrano: using branch 'refucktoring'

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "git@github.com:apsheronets/#{fetch :application}.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, :capistrano
+set :branch, :refucktoring
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'

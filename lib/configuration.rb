f3151f9fd911e89bdcf246cd8505d530419c2779
Alexander Markov <apsheronets@gmail.com>
2016-11-13 05:58:12 +0300

    MultiIO

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -1,3 +1,17 @@
+class MultiIO
+  def initialize(*targets)
+     @targets = targets
+  end
+
+  def write(*args)
+    @targets.each {|t| t.write(*args)}
+  end
+
+  def close
+    @targets.each(&:close)
+  end
+end
+
 module Configuration
   class << self
 
@@ -5,12 +19,13 @@ module Configuration
 
     def new_logger
       env = ENV.fetch('APP_ENV', 'development')
+      logfile = File.join(LOG_DIR, "#{env}.log")
       case env
       when 'development'
-        @logger = Logger.new STDOUT
+        @logger = Logger.new MultiIO.new(STDOUT, File.open(logfile, "a"))
         @logger.level = Logger::DEBUG
       else
-        @logger = Logger.new File.join(LOG_DIR, "#{env}.log")
+        @logger = Logger.new logfile
         @logger.level = Logger::INFO
       end
       return @logger
